cdd84bfb00f425f7283a93a2d7465da0f5199cd6
smv <smitmodi046@gmail.com>
2025-01-17 22:28:43 +0530

    two mathod

--- colorcharger.js
+++ colorcharger.js
@@ -1,50 +1,59 @@
+const buton = document.querySelectorAll('.button')
+const bg_change= document.querySelector('body')
 
-// const buton = document.querySelectorAll('.button')
-// const bg_change= document.querySelector('body')
+bg_change.style.backgroundColor ='red'  
 
-// buton.forEach(function(buton){
-//     buton.addEventListener('click',function(e){
-//         console.log(e.target)
-
-//         function colorchnge(){
-//             bg_change.style.backgroundColor = e.target.id      
-//         }
-//         if(e.target.id === 'grey'){
-
-//             colorchnge() 
-//         }
-//         if(e.target.id === 'white'){
-//             colorchnge() 
-//         }
-//     })
-// })
-
-const buton= document.querySelectorAll('.button')
-const body = document.querySelector('body')
-
-body.style.backgroundColor ='red'   
-
-buton.forEach((select)=> {
-    console.log(select)
-    select.addEventListener('click',function(e){
+buton.forEach(function(buton){
+    buton.addEventListener('click',function(e){
         console.log(e.target)
-        
-        if(e.target.id ==='grey'){
-            body.style.backgroundColor =e.target.id
+
+        function colorchnge(){
+            bg_change.style.backgroundColor = e.target.id      
         }
-        if(e.target.id ==='white'){
-            body.style.backgroundColor =e.target.id
+        if(e.target.id === 'grey'){
+            colorchnge() 
         }
-        if(e.target.id ==='blue'){
-            body.style.backgroundColor =e.target.id
+        if(e.target.id === 'white'){
+            colorchnge() 
+        }
+         if(e.target.id === 'blue'){
+            colorchnge()
         } 
-        if(e.target.id ==='yellow'){
-            body.style.backgroundColor =e.target.id
+        if(e.target.id === 'yellow'){
+            colorchnge()
         } 
         if(e.target.id ==='purple'){
-            body.style.backgroundColor =e.target.id
+            colorchnge()
         } 
     })
 })
+
+// const buton= document.querySelectorAll('.button')
+// const body = document.querySelector('body')
+
+// body.style.backgroundColor ='red'   
+
+// buton.forEach((select)=> {
+//     console.log(select)
+//     select.addEventListener('click',function(e){
+//         console.log(e.target)
+        
+//         if(e.target.id ==='grey'){
+//             body.style.backgroundColor =e.target.id
+//         }
+//         if(e.target.id ==='white'){
+//             body.style.backgroundColor =e.target.id
+//         }
+//         if(e.target.id ==='blue'){
+//             body.style.backgroundColor =e.target.id
+//         } 
+//         if(e.target.id ==='yellow'){
+//             body.style.backgroundColor =e.target.id
+//         } 
+//         if(e.target.id ==='purple'){
+//             body.style.backgroundColor =e.target.id
+//         } 
+//     })
+// })
 
 
